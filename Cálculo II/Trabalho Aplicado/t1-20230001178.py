142f6166aede03411baa4fc75481fe6741ce3d1d
erickson-cc <e.rickson@live.com>
2024-09-21 23:54:19 -0300

    Trabalho Aplicado

--- Cálculo II/Trabalho Aplicado/t1-20230001178.py
+++ Cálculo II/Trabalho Aplicado/t1-20230001178.py
@@ -1,24 +1,38 @@
+#----------------------------------------------------#
+#       Calculadora de Área Integrais Python
+#           Autor: Erickson G. Müller
+#           Data: 23-09-2024
+#           Trabalho Aplicado Cálculo II
+#----------------------------------------------------#           
+
 import sympy
 
-def somaRiemann(funcao, a, b, n):
-  x = sympy.Symbol('x')
-  f = sympy.sympify(funcao)
+def somaRiemann(funcao, a, b, n):#Função 
+    #---Declaração---#
+    x = sympy.Symbol('x')#Atribui x à classe Symbol
+    f = sympy.sympify(funcao)#"Traduz" o input função para linguagem matematica do sympy 
+    #----------------#
 
-  delta_x = (b - a) / n #Largura dos retângulos
+    #----Cálculo-----#
+    delta_x = (b - a) / n #Largura dos retângulos
+    area = 0
+    for i in range(n): #Loop que vai somando as áreas dos retângulos
+        x_i = a + i * delta_x + delta_x / 2 #Ponto médio dos retângulos
+        altura = f.subs(x, x_i)
+        area += altura * delta_x #Área do retângulo da iteração (Base.Altura)
+    #----------------#
+    return area
 
-  area = 0
-  for i in range(n):
-    x_i = a + i * delta_x + delta_x / 2 #Ponto médio dos retângulos
-    altura = f.subs(x, x_i)
-    area += altura * delta_x
-
-  return area
+#---MAIN---#
 
 ###Inputs
-funcao = str(input("escreva a função f(x): "))
-a = int(input("Insira o valor de a: "))
-b = int(input("Insira o valor de b: "))
-n = int(input("insira o valor de n: "))  # Número de retângulos
+funcao = input("escreva a função f(x): ")
+a = int(input("Insira o valor de a: ")) # Limite inferior
+b = int(input("Insira o valor de b: ")) # Limite Superior
+n = int(input("insira o valor de n: "))  # Número de retângulos (subintervalos)
 
-resultado = somaRiemann(funcao, a, b, n)
-print("A área aproximada usando a Soma de Riemann é: {:.3f} unidades de área".format(resultado))
+#----Cálculo----#
+aprox_area = somaRiemann(funcao, a, b, n)
+
+#Resposta$
+print("A área aproximada usando a Soma de Riemann é: {:.6f} unidades de área".format(aprox_area))
