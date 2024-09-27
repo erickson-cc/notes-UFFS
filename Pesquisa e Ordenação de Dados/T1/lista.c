5151f4ae28ad988b22b3cc92ab468a0abe401823
erickson-cc <e.rickson@live.com>
2024-09-27 16:10:29 -0300

    POD

--- Pesquisa e Ordenação de Dados/T1/lista.c
+++ Pesquisa e Ordenação de Dados/T1/lista.c
@@ -1,39 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/* ---Lista--- */
+				/* 
+	---Lista--- 
+.Estrutura dos nodes da lista
+..Sentinela da lista
+...Append_to_List
+				*/
+				//Declaração Lista
 struct tlist{
 	int valor;
-	struct tlist *next;
+	struct tlist* next;
 };
-typedef struct tlist listnode;
+typedef struct tlist nodelist;
+
 
 struct listSent{
-	listnode *head, *tail;
+	nodelist *head, *tail;
 };
-typedef struct listSent sent;
+typedef struct listSent list;
 
-/* ---Fila--- */
+				//Operações Lista
+void appendtoList(list *sent, int i){
+	nodelist *aux;
+	aux = (nodelist *)malloc(sizeof(nodelist));
+	aux->valor = i;
+	aux->next = NULL;
+
+	if(sent->head==NULL){
+		sent->head = aux;
+		sent->tail = aux;
+	}
+	else{
+		sent->tail->next = aux;
+		sent->tail = aux;
+	}
+}
+
+///////////////////////////////////
+
+
+				/*
+	---Fila--- 
+.Estrutura dos nodes da fila
+..Sentinela da fila
+...Enqueue (direto da lista)
+....Dequeue
+				*/
+				// Declaração Fila
 struct tqueue{
 	int valor; 
 	struct tqueue *next;
 };
-typedef struct tqueue queuenode;
+typedef struct tqueue nodefila;
 
-void addNode(list *sent, int i){
-	list *aux;
-	aux = (list *)malloc(sizeof(list));
-}
-
-void printList(sent *head){
-	list *aux;
-	for(aux=sent->head;aux!=NULL;aux=aux->next){
-		printf("%d",aux->valor);
-	}
-}
-int main(){
-	vetor = [-12,34,-7,0,67,-32,89,5,-23,99,-1,42,-50,10,-27,81,-3,65,44,37];
+struct queueSent{
+	nodefila *head, *tail;
+};
+typedef struct queueSent fila;
+				//Operações Fila
+///////////////////////////////////
+void generateVector(int randomMin, int randomMax, int *vector){
+	int randomNumber;
+	int i;
 	for(i=0;i<20;i++){
-		printf("%d",vetor[i]);
+		randomNumber = randomMin+rand() % (randomMax-randomMin+1);
+		vector[i] = randomNumber;
 	}
+}
+	
+int main(){
+	int vector[20];
+	generateVector(-100, 100, vector);
+	int i;
+	for(i=0;i<20;i++){
+		printf(" %d ", vector[i]);
+	}
+
+		
 	return 0;
 }
