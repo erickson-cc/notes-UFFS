4c8c02431579b5c6d41c4b7b18574ea50160fafd
erickson-cc <e.rickson@live.com>
2024-10-01 12:47:12 -0300

    Trabalho 04 Programação I

--- Programação I/Tarefa 7/exerc-2/Main.java
+++ Programação I/Tarefa 7/exerc-2/Main.java
@@ -5,6 +5,13 @@ abstract class Conta{
 		this.numero = numero;
 		this.saldo = saldo;
 	}
+
+	public void Depositar(int valor){
+		this.saldo = this.saldo + valor;
+	}
+	public void ImprimirSaldo(){
+		System.out.println("O saldo da conta "+this.numero+" é: R$ "+this.saldo);
+	}
 }
 
 class ContaCorrente extends Conta{//taxmnt; 
@@ -45,9 +52,23 @@ class ContaAplicacao extends Conta implements AplicarRendimento{//rendimento; di
 
 public class Main{
 	public static void main(String[] args){
+		// Criar os Objetos
 		ContaCorrente conta1 = new ContaCorrente("12345",0,1.0005);
-		ContaPoupanca conta2 = new ContaPoupanca("54321",100,1.008);
-		ContaAplicacao conta3 = new ContaAplicacao("111111",100,1.01);
+		ContaPoupanca conta2 = new ContaPoupanca("54321",0,1.008);
+		ContaAplicacao conta3 = new ContaAplicacao("111111",0,1.01);
+		
+		// Depositar 100 reais
+		conta2.Depositar(100);
+		conta3.Depositar(100);
+
+		// Aplicar Rendimento
+		conta2.AplicarRendimento();
+		conta3.AplicarRendimento();
+
+		// Imprimir Saldo
+		conta1.ImprimirSaldo();
+		conta2.ImprimirSaldo();
+		conta3.ImprimirSaldo();
 	}
 }
 
