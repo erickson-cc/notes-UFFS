ed1cb4ae2b631dbaec4ada40b6c2ab3e17c0d663
erickson-cc <e.rickson@live.com>
2025-08-26 20:35:52 -0300

    T1 BII, pensei na lógica

--- Banco de Dados II/T1/20230001178.c
+++ Banco de Dados II/T1/20230001178.c
@@ -30,7 +30,7 @@
 //    return 0;
 //}
 
-int main(){
+//int main(){
 	// Armazenar nos bytes iniciais o esquema de dados
 	// HEADER:
 	// 	Primeiros 15 bytes representam o nome do atributo (string)
@@ -46,15 +46,45 @@ int main(){
 	// 	val1	val2	val3	val4
 	// 	val1	val2	val3	val4
 	// 	val1	val2	val3	val4
-	
+int main(int argc, char *argv[]){
+	// Verifica se o comando informa o arquivo .dat (POSTERIORMENTE VERIFICAR A EXTENSÃO INFORMADA)
+	if (argc < 2) {
+		printf("Número de argumentos insuficiente: ./main <datafile>\n");
+		return 0;
+	}
+
+	char *tablename = argv[1];
 	
 	// variável char que imprime o documento no terminal
 	char ch;
 	// Cria o ponteiro para o arquivo
 	FILE *fptr;
-	fptr = fopen("customer.dat", "r");
+	fptr = fopen(tablename, "r");
+	if (fptr == NULL) {
+		printf("Erro: Não foi possível abrir o arquivo %s\n", tablename);
+		return 0;
+	}
+
 	while ((ch = fgetc(fptr)) != EOF){
-		printf("%c", ch);
+		// Colocar aqui dentro a interação com o arquivo
+		int i;
+		char metaname[15];
+		char metatype;
+		char metasize[4];
+		//printf("%c", ch);
+		for(i=0;i<15;i++){
+			metaname[i] = fptr[i];
+		}
+		for(i;i<16;i++){
+			metatype = fptr[i];
+		}
+		for(i;i<20;i++){
+			metasize[i] = fptr[i];
+		}
+		printf("%s", metaname);
+		printf("%c", metatype);
+		printf("%s", metasize);
+
 	}
 
 	//fprintf(fptr, "%s", "teste");
