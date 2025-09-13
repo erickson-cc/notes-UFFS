e406aae5788ea0e4f69d4641fedb37186122a50a
erickson-cc <e.rickson@live.com>
2025-09-12 22:03:35 -0300

    Capítulo Impasses

--- Sistemas Operacionais/jantar-dos-filosofos/main.c
+++ Sistemas Operacionais/jantar-dos-filosofos/main.c
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
-#include <semaphore.h>      
+#include <semaphore.h>			
 #include <fcntl.h> 
 
 
@@ -29,38 +29,38 @@ void *mythread(void *data);
 sem_t mutex; // Semáforo real
 sem_t s[N]; //array de threads
 void think(int i){
-  printf("Filósofo %d está pensando\n", i);
-  sleep(rand() % 3+1); // define  um tempo randomico para dormir
+	printf("Filósofo %d está pensando\n", i);
+	sleep(rand() % 3+1); // define	um tempo randomico para dormir
 }
 void take_forks(int i){
-  printf(TAKE_FORKS);
-sem_wait(&mutex);//down
-  state[i] = HUNGRY;
-  printf("Filósof %d está faminto\n", i);
-  test(i);
-  sem_post(&mutex);//up
-  sem_wait(&s[i]);
+	printf(TAKE_FORKS);
+	sem_wait(&mutex);//down
+	state[i] = HUNGRY;
+	printf("Filósof %d está faminto\n", i);
+	test(i);
+	sem_post(&mutex);//up
+	sem_wait(&s[i]);
 }
 void eat(){
-  printf("Filósofo %d está está comendo\n", i);
-  sleep(rand() % 3+1);
+	printf("Filósofo %d está está comendo\n", i);
+	sleep(rand() % 3+1);
 }
 
 void put_forks(i){
-  printf(PUT_FORKS);
-  sem_wait(&mutex);
-  state[i] = THINKING;
-  printf("Filósofo %d está colocando garfos\n", i);
-  test(LEFT);
-  test(RIGHT);
-  sem_post(&mutex);
+	printf(PUT_FORKS);
+	sem_wait(&mutex);
+	state[i] = THINKING;
+	printf("Filósofo %d está colocando garfos\n", i);
+	test(LEFT);
+	test(RIGHT);
+	sem_post(&mutex);
 }
 
 void test(i){
-  if(state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){
-    state[i] = EATING;
-    up(&s[i]);
-  }
+	if(state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){
+		state[i] = EATING;
+		sem_post(&s[i]);
+	}
 }
 
 
@@ -70,46 +70,46 @@ pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 
-//void philosopher(int i){
-//  while(TRUE){
-//    think();
-//    take_forks(i);
-//    eat();
-//    put_forks(i);
-//  }
-//}
-
-
-void *philosopher_thread(void *data){
-  int i = *(int*)data;
-  free(data);
-  while(TRUE){
-    think(i);
-    take_forks(i);
-    eat(i);
-    put_forks(i);
-  }
-  return NULL;
+void philosopher(int i){
+	while(TRUE){
+		think();
+		take_forks(i);
+		eat();
+		put_forks(i);
+	}
 }
 
 
+//void *philosopher_thread(void *data){
+//	int i = *(int*)data;
+//	free(data);
+//	while(TRUE){
+//		think(i);
+//		take_forks(i);
+//		eat(i);
+//		put_forks(i);
+//	}
+//	return NULL;
+//}
+
+
 int main() {
-   pthread_t tids[N]; // define as 5 threads
-   int i=0;
+	pthread_t tids[N]; // define as 5 threads
+	int i=0;
 
-   sem_init(&mutex, 0, 1); // inicializa o semáforo com valor 1
+	sem_init(&mutex, 0, 1); // inicializa o semáforo com valor 1
 
-   for(i=0; i<N; i++) {
-      int *j = malloc(sizeof(int));
-      *j = i;
-      pthread_create(&tids[i], NULL, mythread, (void *)j); // cria as thread
-   }
-   /* We will now wait for each thread to<\n>
-    * terminate */
-   for(i=0; i<N; i++) {
-      pthread_join(tids[i], NULL);
-      printf("Thread id %ld returned\n", tids[i]);
-   }
-   return 1;
+	for(i=0; i<N; i++) {	
+	      int *j = malloc(sizeof(int));
+	      *j = i;
+	      pthread_create(&tids[i], NULL, mythread, (void *)j); // cria as thread
+	}
+	/* We will now wait for each thread to<\n>
+	      * terminate */
+	for(i=0; i<N; i++) {
+	      pthread_join(tids[i], NULL);
+	      printf("Thread id %ld returned\n", tids[i]);
+	}
+	return 1;
 }
 
