6bceda6f61d68f04988c5b9fa5f419578ebb77d1
erickson-cc <e.rickson@live.com>
2025-10-23 21:41:35 -0300

    Tanenbaum Jantar dos filosofos

--- Sistemas Operacionais/jantar-dos-filosofos/main.c
+++ Sistemas Operacionais/jantar-dos-filosofos/main.c
@@ -13,50 +13,51 @@
 #include <sys/time.h>
 #include <semaphore.h>			
 #include <fcntl.h> 
+#include <errno.h>
 
 
-void *mythread(void *data);
 
-#define N 5 // number of threads
-#define TRUE 1
-#define TAKE_FORKS "has taken a fork\n"
-#define PUT_FORKS "is putting forks\n"
 #define THINKING 0
 #define HUNGRY 1
 #define EATING 2
+
+#define N 5 // number of threads
+#define TRUE 1
 #define LEFT (i+N-1)%N
 #define RIGHT (i+1)%N 
 sem_t mutex; // Semáforo real
 sem_t s[N]; //array de threads
+int state[N];
+void *philosopher(void *data);
+void take_forks(int i);
+void put_forks(int i);
+void test(int i);
+
 void think(int i){
-	printf("Filósofo %d está pensando\n", i);
+	printf("Philosopher %d is thinking\n", i);
 	sleep(rand() % 3+1); // define	um tempo randomico para dormir
 }
 void take_forks(int i){
-	printf(TAKE_FORKS);
+	printf("Philosofer %d has taken a fork\n",i);
 	sem_wait(&mutex);//down
 	state[i] = HUNGRY;
-	printf("Filósof %d está faminto\n", i);
+	printf("Philosopher %d is hungry\n", i);
 	test(i);
 	sem_post(&mutex);//up
 	sem_wait(&s[i]);
 }
-void eat(){
-	printf("Filósofo %d está está comendo\n", i);
-	sleep(rand() % 3+1);
-}
 
-void put_forks(i){
-	printf(PUT_FORKS);
+void put_forks(int i){
+	printf("Philosofer %d has taken a fork\n",i);
 	sem_wait(&mutex);
 	state[i] = THINKING;
-	printf("Filósofo %d está colocando garfos\n", i);
+	printf("Philosopher %d is putting forks\n", i);
 	test(LEFT);
 	test(RIGHT);
 	sem_post(&mutex);
 }
 
-void test(i){
+void test(int i){
 	if(state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){
 		state[i] = EATING;
 		sem_post(&s[i]);
@@ -66,28 +67,30 @@ void test(i){
 
 
 
-pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
+//pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 
-void philosopher(int i){
-	while(TRUE){
-		think();
-		take_forks(i);
-		eat();
-		put_forks(i);
+void *philosopher(void *data){
+	int id = *((int *) data);
+	while(1){
+		printf("Philosopher nº %d is thinking\n",id);
+		sleep(1);
+		take_forks(id);
+		sleep(1);
+		printf("Philosopher nº %d is eating\n",id);
+		put_forks(id);
 	}
 }
-
 
 //void *philosopher_thread(void *data){
 //	int i = *(int*)data;
 //	free(data);
 //	while(TRUE){
-//		think(i);
-//		take_forks(i);
-//		eat(i);
-//		put_forks(i);
+//		think(id);
+//		take_forks(id);
+//		eat(id);
+//		put_forks(id);
 //	}
 //	return NULL;
 //}
@@ -100,9 +103,14 @@ int main() {
 	sem_init(&mutex, 0, 1); // inicializa o semáforo com valor 1
 
 	for(i=0; i<N; i++) {	
+		sem_init(&s[i],0,0);
+		state[i]=THINKING;
+	}
+	for(i=0; i<N; i++) {	
 	      int *j = malloc(sizeof(int));
 	      *j = i;
-	      pthread_create(&tids[i], NULL, mythread, (void *)j); // cria as thread
+	      printf("Creating philosopher %d\n", *j);
+	      pthread_create(&tids[i], NULL, philosopher, (void *)j); // cria as thread
 	}
 	/* We will now wait for each thread to<\n>
 	      * terminate */
@@ -110,6 +118,7 @@ int main() {
 	      pthread_join(tids[i], NULL);
 	      printf("Thread id %ld returned\n", tids[i]);
 	}
+
 	return 1;
 }
 
---
+++ Sistemas Operacionais/jantar-dos-filosofos/tanenbaum.c
@@ -0,0 +1,52 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <semaphore.h>			
+#include <fcntl.h> 
+#include <errno.h>
+#define N 5
+#define LEFT (i+N-1)%N
+#define RIGHT (i+1)%N
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
+
+typedef int semaphore;
+int state[N];
+semaphore mutex=1;
+semaphore s[N];
+
+void philosopher(int i){
+	while(1==1){
+		think();
+		take_forks(i);
+		eat();
+		put_forks(i);
+	}
+}
+
+void take_forks(int i){
+	down(&mutex);
+	state[i] = HUNGRY;
+	test(i);
+	up(&mutex);
+	down(&s[i]);
+}
+
+void put_forks(i){
+	down(&mutex);
+	state[i] = THINKING;
+	test(LEFT);
+	test(RIGHT);
+	up(&mutex);
+}
+
+void test(i){
+	if (state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){
+		state[i] = EATING;
+		up(&s[i]);
+	}
+}
+
