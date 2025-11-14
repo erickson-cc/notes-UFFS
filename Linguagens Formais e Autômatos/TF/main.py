0915ffae3dc9a8e316b05d5d01c89c29c88467d3
erickson-cc <e.rickson@live.com>
2025-11-13 21:30:26 -0300

    Inicio do trabalho e revisão da prova

---
+++ Linguagens Formais e Autômatos/TF/main.py
@@ -0,0 +1,3 @@
+# Gerar um AFD livre de estados inalcançáveis e mortos
+# Entrada: Arquivo com a relação de tokens e/ou GRs de uma linguagem
+# Saída: AFD e mínimo sem a aplicação de classes de equivalência entre estados.
