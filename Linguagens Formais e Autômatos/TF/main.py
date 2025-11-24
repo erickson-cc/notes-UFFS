40d693e32da1b98295c0b38aaa727076391c6a48
erickson-cc <e.rickson@live.com>
2025-11-23 22:37:22 -0300

    Trabalho final

--- Linguagens Formais e Autômatos/TF/main.py
+++ Linguagens Formais e Autômatos/TF/main.py
@@ -1,3 +1,13 @@
 # Gerar um AFD livre de estados inalcançáveis e mortos
 # Entrada: Arquivo com a relação de tokens e/ou GRs de uma linguagem
 # Saída: AFD e mínimo sem a aplicação de classes de equivalência entre estados.
+
+def iterarLinha(linha):
+    linha = linha.strip() # quebra de linha
+    if not linha:
+        return
+
+    if "::=" in linha:
+        processarGramatica(linha)
+    else
+        processarToken(linha)
