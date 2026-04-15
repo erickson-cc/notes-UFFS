70b84d9a961b67acc3550d44f1ea11b39f885d23
erickson-cc <e.rickson@live.com>
2026-04-15 00:23:31 -0300

    inicio relatório e inicio mensagem de erro

--- Construção de Compiladores/project-1/main.py
+++ Construção de Compiladores/project-1/main.py
@@ -1,21 +1,21 @@
-# Autor: Erickson Giesel M�ller
-# Matrícula: 20230001178
+# Autor: Erickson Giesel Müller
+# Matricula: 20230001178
 
-#------------vvvvvvvvv  VARIÁVEIS GLOBAIS  vvvvvvvvv------------
+#------------vvvvvvvvv  VARIAVEIS GLOBAIS  vvvvvvvvv------------
 afnd = {}
 estados_finais = set()      # conjunto para guardar estados finais
-contador_estados = -1        # Variável para gerar nomes de estados (A-B-C-D-E...)
+contador_estados = -1        # variavel para gerar nomes de estados (A-B-C-D-E...)
 mapa_gramatica = {"S":"S"}
 rotulos_originais = {}
 arquivo = "fonte.txt"
 entrada = "entrada.txt"
 
-#------------vvvvvvvvv CRIAÇÃO DO AUTÔMATO vvvvvvvvv------------
+#------------vvvvvvvvv CRIACAO DO AUTOMATO vvvvvvvvv------------
 def processarGramatica(linha):
-    # Apenas o estado inicial <S> é compartilhado por todas as regras
+    # Apenas o estado inicial <S> e compartilhado por todas as regras
     # global contador_estados
-    print("Processando Gramática na linha:"+linha+"...")
-    lados = linha.split("::=") #[,] esquerdo = <S> ; direito = a<A> | b<A> | ε
+    print("Processando Gramatica na linha:"+linha+"...")
+    lados = linha.split("::=") #[,] esquerdo = <S> ; direito = a<A> | b<A> | epsilon
     nome_origem = lados[0].strip().replace("<","").replace(">","")
 
     if nome_origem in mapa_gramatica:
@@ -28,22 +28,22 @@ def processarGramatica(linha):
     producoes = direito.split("|")
 
     if esquerdo not in afnd:
-        # se o estado ainda não existe no dicionário
+        # se o estado ainda nao existe no dicionario
         afnd[esquerdo] = {}
 
     for p in producoes:
         p = p.strip()
 
-        if p == "ε": # Caso seja produção vazia
+        if p == "ε": # Caso seja produçao vazia
             estados_finais.add(esquerdo)
-            rotulos_originais[esquerdo] = "Terminal" # colocar no relatorio
+            rotulos_originais[esquerdo] = "variavel" # colocar no relatorio
             continue
         elif "<" in p and ">" in p: # Caso seja x<X>
             pedacos = p.split("<") # separa o lado esquerdo do direito
             simbolo = pedacos[0].strip()
             nome_destino = pedacos[1].replace(">","").strip() # tira o estado destino
             if nome_destino in mapa_gramatica:
-                # testar para ver se não está interferindo nos estados
+                # testar para ver se nao esta interferindo nos estados
                 # o ideal seria criar sempre um novo estado ?
                 estado_destino = mapa_gramatica[nome_destino]
             else:
@@ -56,7 +56,7 @@ def processarGramatica(linha):
             afnd[esquerdo][simbolo].append(estado_destino)
 
         elif "<" not in p and ">" not in p and len(p)==1:
-            # if caso a produção seja apenas terminal
+            # if caso a producao seja apenas terminal
             simbolo = p
             novo_estado = obterProxNome()
             if novo_estado not in afnd:
@@ -65,11 +65,11 @@ def processarGramatica(linha):
                afnd[esquerdo][simbolo] = []
             afnd[esquerdo][simbolo].append(novo_estado)
             estados_finais.add(novo_estado)
-            rotulos_originais[esquerdo] = "Terminal" # colocar no relatorio
+            rotulos_originais[esquerdo] = "variavel" # colocar no relatorio
             
 
         else:
-            print("Problema na leitura das produções")
+            print("Problema na leitura das producoes")
             break
 
     print(f"Processamento do estado {esquerdo} finalizado.")
@@ -91,26 +91,26 @@ def processarToken(linha):
         if novo_estado not in afnd:
             # inicializar novo estado
             afnd[novo_estado] = {}
-        # Transição atual->novo
+        # transicao atual->novo
         if c not in afnd[estado_atual]:
-            #  caso a letra não está na lista de transições
+            #  caso a letra nao esta na lista de transicoes
             afnd[estado_atual][c] = []
         ##
         afnd[estado_atual][c].append(novo_estado)
 
         estado_atual = novo_estado
     estados_finais.add(estado_atual)
-    rotulos_originais[estado_atual] = f"Palavra reservada '{token}'" # colocar no relatorio (guarda a label do estado)
+    rotulos_originais[estado_atual] = "palavra reservada" # colocar no relatorio (guarda a label do estado)
     print(f"Processamento do token '{token}' finalizado.")
 
 def gerarNomeEstado(n):
     nome = ""
     while n>=0:
-        nome = chr(n%26+65)+nome # gera o estado em ordem alfabética
+        nome = chr(n%26+65)+nome # gera o estado em ordem alfabetica
         n = n//26 -1 # caso n>26 -> nome=AA,AB,AC...
 
     if nome == "S":
-        nome = gerarNomeEstado(n+1) # o estado S é fixo para o estado inicial
+        nome = gerarNomeEstado(n+1) # o estado S e fixo para o estado inicial
 
     return nome
 
@@ -133,8 +133,8 @@ def iterarLinha(linha):
         processarGramatica(linha)
     else:
         processarToken(linha)
-#------------^^^^^^^^^ CRIAÇÃO DO AUTÔMATO ^^^^^^^^^------------
-#------------vvvvvvvvv       IMPRESSÃO     vvvvvvvvv------------
+#------------^^^^^^^^^ CRIACAO DO AUTOMATO ^^^^^^^^^------------
+#------------vvvvvvvvv       IMPRESSAO     vvvvvvvvv------------
 def alfabetoAFND(afnd):
     alfabeto = set()
     for estado in afnd:
@@ -144,13 +144,13 @@ def alfabetoAFND(afnd):
 
 def imprimir_automato(automato, finais):
     print("-" * 50)
-    print(f"ESTADO   | TRANSIÇÕES")
+    print(f"ESTADO   | TRANSICOES")
     print("-" * 50)
 
-    # Ordena para 'S' aparecer primeiro ou ficar alfabético
+    # Ordena para 'S' aparecer primeiro ou ficar alfabetico
     estados_ordenados = sorted(automato.keys())
     
-    # Se 'S' estiver na lista, move para o início
+    # Se 'S' estiver na lista, move para o inicio
     if 'S' in estados_ordenados:
         estados_ordenados.remove('S')
         estados_ordenados.insert(0, 'S')
@@ -162,7 +162,7 @@ def imprimir_automato(automato, finais):
         
         nome_formatado = f"{prefixo_inicial}{prefixo_final}{estado}"
         
-        # Monta string das transições
+        # Monta string das transicoes
         lista_transicoes = []
         if estado in automato:
             for simbolo, destinos in automato[estado].items():
@@ -176,13 +176,13 @@ def imprimir_automato(automato, finais):
     
     print("="*50 + "\n")
 
-#------------^^^^^^^^^       IMPRESSÃO     ^^^^^^^^^------------
-#------------vvvvvvvvv    DETERMINIZAÇÃO   vvvvvvvvv------------
+#------------^^^^^^^^^       IMPRESSAO     ^^^^^^^^^------------
+#------------vvvvvvvvv    DETERMINIZACAO   vvvvvvvvv------------
 def determinizar(afnd, estados_finais):
-    print("Iniciando determinização do AFND...")
+    print("Iniciando determinizacao do AFND...")
     afd = {}
     estado_inicial = frozenset(['S']) # congela a lista
-    fila = [estado_inicial] # fila de processamento de estados que ainda não foram analisados
+    fila = [estado_inicial] # fila de processamento de estados que ainda nao foram analisados
     estados_processados = set()
     estados_processados.add(estado_inicial) # para evitar loop infinito processando o mesmo estado composto infinitas vezes
     estados_finais_afd = set() # estados finais determinizados
@@ -191,19 +191,19 @@ def determinizar(afnd, estados_finais):
 
     while fila:
         conjunto_atual = fila.pop(0) # next
-        nome_estado = "_".join(sorted(list(conjunto_atual))) # [A_B], tentei adicionar os "[]"+"[]" mas isso dificulta a impressão
+        nome_estado = "_".join(sorted(list(conjunto_atual))) # [A_B], tentei adicionar os "[]"+"[]" mas isso dificulta a impressao
 
         if nome_estado not in afd:
-            # se o estado composto ainda não existe no dicionário, cria
+            # se o estado composto ainda nao existe no dicionario, cria
             afd[nome_estado] = {}
 
         for i in conjunto_atual:
-            #verifica se o estado composto é final, se algum dos estados do conjunto_atual é final
+            #verifica se o estado composto e final, se algum dos estados do conjunto_atual e final
             if i in estados_finais:
                 estados_finais_afd.add(nome_estado)
                 if i in rotulos_originais: # logica das labels do afd
                     rotulos_afd[nome_estado] = rotulos_originais[i]
-                    if "Palavra reservada" in rotulos_originais[i]:
+                    if "palavra reservada" in rotulos_originais[i]:
                         break
 
         for letra in alfabeto:
@@ -211,40 +211,40 @@ def determinizar(afnd, estados_finais):
             destinos = set()
             for subestado in conjunto_atual:
                 if subestado in afnd and letra in afnd[subestado]:
-                    # se o subestado tem transição com essa letra
-                    # pega os destinos possíveis no AFND
+                    # se o subestado tem transicao com essa letra
+                    # pega os destinos possiveis no AFND
                     lista_destinos = afnd[subestado][letra]
-                    # adiciona oa conjunto de destinos do novo estaod composto
+                    # adiciona oa conjunto de destinos do novo estado composto
                     destinos.update(lista_destinos)
 
             if destinos: # se encontrou destino para o terminal
                 novo_estado_composto = frozenset(destinos)
                 nome_destino = "_".join(sorted(list(novo_estado_composto)))
-                # cria a transição do afd A -x-> B
-                # como no AFD o destino é único, é usada uma lista de 1 elemento
+                # cria a transicao do afd A -x-> B
+                # como no AFD o destino e unico, e usada uma lista de 1 elemento
                 afd[nome_estado][letra] = [nome_destino]
 
                 if novo_estado_composto not in estados_processados:
-                    # se o estado composto ainda não foi processado Estado Novo
+                    # se o estado composto ainda nao foi processado Estado Novo
                     # marca como visitado e adiciona na fila para ser processado
                     estados_processados.add(novo_estado_composto)
                     fila.append(novo_estado_composto)
     return afd, estados_finais_afd, rotulos_afd
 
-#------------^^^^^^^^^    DETERMINIZAÇÃO   ^^^^^^^^^------------
+#------------^^^^^^^^^    DETERMINIZACAO   ^^^^^^^^^------------
 #------------vvvvvvvvv    ESTADO DE ERRO   vvvvvvvvv------------
 def addEstadoErro(afd, alfabeto, estados_finais):
-    nome_erro = "_"# para melhor visualizar, o estado de erro é apenas um underline
+    nome_erro = "_"# para melhor visualizar, o estado de erro e apenas um underline
     p_erro = False
 
     # Listar estados existentes
     estados_existentes = list(afd.keys())
-    # Percorrer o autômato procurando falhas
+    # Percorrer o automato procurando falhas
     for estado in estados_existentes:
         for letra in alfabeto:
             if letra not in afd[estado]:
                 p_erro = True
-                afd[estado][letra] = [nome_erro] #transição para o erro
+                afd[estado][letra] = [nome_erro] #transicao para o erro
 
     # definir o estado ERRO
     if p_erro:
@@ -252,33 +252,34 @@ def addEstadoErro(afd, alfabeto, estados_finais):
         for letra in alfabeto:
             afd[nome_erro][letra] = [nome_erro]
     #
-    # estados_finais.add(nome_erro) # Removido pois não add o estado de erro ao estado de aceitação
+    # estados_finais.add(nome_erro) # Removido pois nao add o estado de erro ao estado de aceitacao
     return afd, estados_finais
 
-#------------vvvvvvvvv     ANALISADOR LÉXICO     vvvvvvvvv------------
+#------------vvvvvvvvv     ANALISADOR LEXICO     vvvvvvvvv------------
 def ler_entrada():
     try:
         with open(entrada, 'r', encoding='utf-8') as entradaanal:
             conteudo = entradaanal.read()
     except FileNotFoundError:
-        print("Erro: O arquivo"+entrada+"não foi encontrado")
+        print("Erro: O arquivo"+entrada+"nao foi encontrado")
         return
     return conteudo
 def imprimir_fita(fita):
     print(f"\nFita: {' '.join(fita)} $")
 def imprimir_ts(tabela_simbolos):
-    print("------ TABELA DE SIMBOLOS ------")
-    print(f"{'Linha':<7} | {'Identificador':<15} | {'Rotulo'}")
-    print("_"*40)
+    print("-"*25+" TABELA DE SIMBOLOS "+"-"*25)
+    print(f"{'Linha':<7} | {'Id':<3} | {'Rotulo':<17} | {'Mensagem':<17} | {'Palavra'}")
+    print("_"*70)
     for ts in tabela_simbolos:
-        print(f"{ts['linha']:<7} | {ts['identificador']:<15} | {ts['label']}")
-        print("_"*40)
+        print(f"{ts['linha']:<7} | {ts['identificador']:<3} | {ts['label']:<17} | {ts['mensagem']:<17} | {ts['palavra']}")
+        print("_"*70)
 def analisadorLex(entrada, afd, finais_afd, rotulos_afd):
     fita = []
     tabela_simbolos = []
     linha_atual = 1
     conteudo = ler_entrada()
     i = 0
+    mensagem_erro = ""
     tamanho = len(conteudo)
     while i<tamanho:
         estado_corrente = "S"
@@ -286,41 +287,52 @@ def analisadorLex(entrada, afd, finais_afd, rotulos_afd):
             if conteudo[i] == "\n":
                 linha_atual += 1
             i += 1
-        # Condição de parada
+        # Condicao de parada
         if i>= tamanho:
             break
+
+        inicio_token = i
 
         # leitura do token
         while i < tamanho:
             simbolo_lex = conteudo[i]
             if simbolo_lex in [" ", "\n"]:
                 break # fim do token
-            #transição
+            #transicao
             if estado_corrente in afd and simbolo_lex in afd[estado_corrente]:
                 estado_corrente = afd[estado_corrente][simbolo_lex][0]
-                # a linha 219 do código transforma o estado em uma lista, o índice evita analisar a lista inteira
+                # a linha 216 do codigo transforma o estado em uma lista, o ndice evita analisar a lista inteira
             else:
-                estado_corrente = "_" # estado de erro se o terminal não tem transição
+                estado_corrente = "_" # estado de erro se o terminal nao tem transicao
+                                    # vai seguir no estado _ até encontrar um separador
             i+= 1
 
+        palavra_lida = conteudo[inicio_token:i]
+
         if estado_corrente not in finais_afd:
-            estado_corrente = "_"  # EC = X pois não final
+            estado_corrente = "_"  # EC = X pois nao final
 
         fita.append(estado_corrente)
         
         # Logica de label na  tabela
         if estado_corrente == "_":
-            rotulo_final = "Erro"
+            rotulo_final = "erro"
         else:
-            rotulo_final = rotulos_afd.get(estado_corrente, "Indefinido")
+            rotulo_temp = rotulos_afd.get(estado_corrente, "Indefinido")
+            if "variavel" in rotulo_temp:   # variavel
+                rotulo_final = "variavel"
+            else:                           # palavra reservada
+                rotulo_final = rotulo_temp
 
         tabela_simbolos.append({
             "linha": linha_atual,
             "identificador": estado_corrente,
-            "label": rotulo_final
+            "label": rotulo_final,
+            "mensagem": mensagem_erro,
+            "palavra": palavra_lida
             })
-    imprimir_fita(fita)
     imprimir_ts(tabela_simbolos)
+    imprimir_fita(fita)
                 
 
 
@@ -335,15 +347,16 @@ try:
     # print(f"\nEstados finais: {estados_finais}")
     # imprimir_automato(afnd, estados_finais)
     # chamada da det
-    print("AFND Construído")
+    print("AFND Construido")
     afd, finais_afd, rotulos_afd = determinizar(afnd, estados_finais)
     #chamada estado_erro
     alfabeto_lista = alfabetoAFND(afnd)
     afd, finais_afd = addEstadoErro(afd, alfabeto_lista, finais_afd)
     print("AFND foi determinizado")
-    # imprimir_automato(afd, finais_afd)
+    imprimir_automato(afd, finais_afd)
     # print(f"Estados Finais AFD: {finais_afd}")
     analisadorLex(entrada, afd, finais_afd, rotulos_afd)
     
 except FileNotFoundError:
-    print("Erro: O arquivo"+arquivo+"não foi encontrado")
+    print("Erro: O arquivo"+arquivo+"nao foi encontrado")
+
